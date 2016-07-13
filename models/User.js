9bc20efde9238fe546b282290c35a734b60ccebd
thanhlbk <thanhlbk@rikkeisoft.com>
2016-07-13 09:42:55 +0700

    remove social field

--- models/User.js
+++ models/User.js
@@ -6,24 +6,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   passwordResetToken: String,
-  passwordResetExpires: Date,
-
-  facebook: String,
-  twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
-  steam: String,
-  tokens: Array,
-
-  profile: {
-    name: { type: String, default: '' },
-    gender: { type: String, default: '' },
-    location: { type: String, default: '' },
-    website: { type: String, default: '' },
-    picture: { type: String, default: '' }
-  }
+  passwordResetExpires: Date
+  
 }, { timestamps: true });
 
 /**
